ba366b13c64d6e7ef877b0eb5eb9b469a5492abc
Tyler Fabian <tylerf808@gmail.com>
2025-12-15 16:26:14 -0500

    Added checkbox elements to accordions. Added header to the jobs table.

--- src/pages/viewJobs/Accordion.jsx
+++ src/pages/viewJobs/Accordion.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import formatUSD from "../../helpers/currencyFormatter"
 
 export default function Accordion({ job }) {
 
@@ -11,27 +12,29 @@ export default function Accordion({ job }) {
     }
 
     return (
-        <div onClick={(e) => toggleAccordion(e)} className={accordionOpen ? 'flex flex-col self-center justify-self-center justify-between w-11/12 h-20 hover:cursor-pointer transition-all duration-75'
-            : 'flex flex-col self-center justify-self-center justify-between w-11/12 h-10 hover:cursor-pointer transition-all duration-75'}>
-            <div id="accordion-header" className="flex flex-row justify-between w-11/12">
-                <div id="accordion-left-header" className="flex flex-row justify-evenly w-auto">
-                    <input id="accordion-checkbox" type="checkbox" className="mr-3"></input>
-                    <h2 className="font-semibold text-sm">Date <span className="m-2 font-light text-sm">{job.date}</span></h2>
-                    <h2 className="font-semibold text-sm">Start <span className="m-2 font-light text-sm">{job.start}</span></h2>
-                    <h2 className="font-semibold text-sm">Drop Off <span className="m-2 font-light text-sm">{job.end}</span></h2>
+        <div onClick={(e) => toggleAccordion(e)} className={accordionOpen ? 'grid grid-rows-2 w-11/12 h-20 hover:cursor-pointer transition-all duration-75'
+            : 'grid grid-rows-1 w-11/12 h-12 hover:cursor-pointer transition-all duration-75'}>
+            <div id="accordion-header" className="grid grid-cols-[10%,10%,10%,20%,20%,30%] items-center w-auto">
+                <h2 className="font-semibold text-sm mr-4">{job.date}</h2>
+                <h2 className="font-semibold text-sm mr-4">{job.client}</h2>
+                <h2 className="font-semibold text-sm mr-4">{job.driver}</h2>
+                <h2 className="font-semibold text-sm mr-4">{job.start}</h2>
+                <h2 className="font-semibold text-sm">{job.end}</h2>
+                <div id="accordion-right-header" className="grid grid-cols-3 justify-content-center w-auto">
+                    <p className='text-center'>{formatUSD(job.revenue)}</p>
+                    <p className='text-center'>{formatUSD(job.totalCost)}</p>
+                    <p className='text-center'>{formatUSD(job.netProfit)}</p>
                 </div>
-                {/* <div id="accordion-right-header" className="flex flex-row justify-evenly w-auto">
-                    <p>Revenue <span>{job.revenue}</span></p>
-                    <p>Total Costs <span>{job.totalCost}</span></p>
-                    <p>Net Profit <span>{job.netProfit}</span></p>
-                </div> */}
             </div>
-            <div id="accordion-body" className={accordionOpen ? 'flex flex-row justify-evenly w-11/12 h-auto transition-all duration-75 opacity-100'
-                : 'flex flex-row justify-evenly w-11/12 h-0 transition-all duration-75 opacity-0'}>
-                <p>Body Item</p>
-                <p>Body Item</p>
-                <p>Body Item</p>
-            </div>
+            {accordionOpen ?
+                <div id="accordion-body" className={accordionOpen ? 'grid grid-cols-3 justify-evenly w-11/12 h-auto transition-all duration-75 opacity-100'
+                    : 'hidden  transition-all duration-75 opacity-0'}>
+                    <p className='text-center'>Body Item</p>
+                    <p className='text-center'>Body Item</p>
+                    <p className='text-center'>Body Item</p>
+                </div>
+                :
+                null}
         </div>
     )
 }
--- src/pages/viewJobs/JobsTable.jsx
+++ src/pages/viewJobs/JobsTable.jsx
@@ -31,18 +31,29 @@ const JobsTable = ({ jobs, selectedJobs, setSelectedJobs }) => {
     };
 
     return (
-        <div className='jobs-table'>
-            <div id='jobs-table-header'>
-                <h2>Date</h2>
-                <h2>Start</h2>
-                <h2>End</h2>
+        <div id='jobs-table' className="h-max mt-14">
+            <div id='jobs-table-header' className='grid grid-cols-[10%,10%,10%,20%,20%,30%] justify-items-start justify-self-center w-11/12'>
+                <h2 className='ml-7'>Date</h2>
+                <h2 className='ml-7'>Client</h2>
+                <h2 className='ml-7'>Driver</h2>
+                <h2 className='ml-7'>Start</h2>
+                <h2 className='ml-7'>End</h2>
+                <div className='grid grid-cols-3 justify-items-center w-full'>
+                    <h2 className='text-center ml-7'>Revenue</h2>
+                    <h2 className='text-center ml-7'>Total Costs</h2>
+                    <h2 className='text-center ml-7'>Net Profit</h2>
+                </div>
             </div>
-            {jobs.map((job) => {
-                return (
-                    <Accordion job={job} />
-                )
-            })}
-
+            <div id='jobs-table-body' className='overflow-y-hidden h-full'>
+                {jobs.map((job) => {
+                    return (
+                        <div id='accordion-row' className='w-full flex flex-row justify-center'>
+                            <input type='checkbox' id='accordion-checkbox' className='self-center mr-4'></input>
+                            <Accordion job={job} />
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 };
